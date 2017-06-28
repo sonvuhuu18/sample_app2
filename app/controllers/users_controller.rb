ad135f22c33195b00b68c76ab1e80b9c012df8bc
sonvuhuu18 <son.vuhuu18@gmail.com>
2017-06-28 15:39:00 +0700

    Update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create, :show]
+  before_action :find_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.per_page
+  end
+
   def new
     @user = User.new
   end
@@ -14,18 +23,52 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    unless @user
-      flash[:danger] = t "not_found"
-      redirect_to root_path
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      render :edit
     end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t "user_deleted"
+    redirect_to users_url
   end
 
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
-      :password_confirmation
+      :password_confirmation, :gender
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "please_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless @user.current_user? current_user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "not_found"
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,10 @@ class User < ApplicationRecord
     end
   end
 
+  def current_user? user
+    self == user
+  end
+
   def remember
     self.remember_token = User.new_token
     update_attributes remember_digest: User.digest(remember_token)
